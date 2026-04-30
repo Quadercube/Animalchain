c444fc70b62d8285aedf5204b4efb26ed4c3450e
Quadercube <pavlo@tschebull.net>
2026-05-01 00:49:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
-// js/config.js
-// Hier Supabase Project URL und anon/publishable key eintragen.
-// Niemals service_role key oder database password hier einfügen.
+
 const ANIMALCHAIN_CONFIG = {
   supabaseUrl: "https://xbncxguszajafewaullp.supabase.co",
   supabaseKey: "sb_publishable_cft_HvPmZgUTVRKI8aFYTg_YMO4HnNF"
