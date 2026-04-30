f3ea6d64c7f5d6ee49ea6a67a24160d39b8cd5ec
moboop <moritz.walland09@gmail.com>
2026-05-01 00:54:01 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,9 @@
-
-const ANIMALCHAIN_CONFIG = {
+jsconst ANIMALCHAIN_CONFIG = {
   supabaseUrl: "https://xbncxguszajafewaullp.supabase.co",
   supabaseKey: "sb_publishable_cft_HvPmZgUTVRKI8aFYTg_YMO4HnNF"
 };
- 
+
 const supabaseClient = window.supabase.createClient(
   ANIMALCHAIN_CONFIG.supabaseUrl,
   ANIMALCHAIN_CONFIG.supabaseKey
 );
- 
